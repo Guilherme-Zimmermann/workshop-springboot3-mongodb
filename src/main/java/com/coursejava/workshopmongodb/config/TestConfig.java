8a92e145f2bc670b607838ca5a45a25e3d506ae9
GuiWill06 <zimmerma83@gmail.com>
2023-08-03 19:57:14 -0400

    AuthorDTO

--- src/main/java/com/coursejava/workshopmongodb/config/TestConfig.java
+++ src/main/java/com/coursejava/workshopmongodb/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.coursejava.workshopmongodb.domain.Post;
 import com.coursejava.workshopmongodb.domain.User;
+import com.coursejava.workshopmongodb.dto.AuthorDTO;
 import com.coursejava.workshopmongodb.repositories.PostRepository;
 import com.coursejava.workshopmongodb.repositories.UserRepository;
 
@@ -35,10 +36,11 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Will", "will@gmail.com");
 		User u3 = new User(null, "Zimmermann", "zimmermann@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("01/08/2023"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-		Post post2 = new Post(null, sdf.parse("26/07/2023"), "Bom dia", "Acordei feliz hoje", u2);
-		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		
+		Post post1 = new Post(null, sdf.parse("01/08/2023"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+		Post post2 = new Post(null, sdf.parse("26/07/2023"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(u2));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
