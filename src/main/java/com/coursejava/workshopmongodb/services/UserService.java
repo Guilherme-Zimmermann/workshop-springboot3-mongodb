f25f34c2d7062f90bc84b1f1279407f6e7e41774
GuiWill06 <zimmerma83@gmail.com>
2023-08-03 18:15:58 -0400

    Get User by id

--- src/main/java/com/coursejava/workshopmongodb/services/UserService.java
+++ src/main/java/com/coursejava/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.coursejava.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coursejava.workshopmongodb.domain.User;
 import com.coursejava.workshopmongodb.repositories.UserRepository;
+import com.coursejava.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("This id not exist!"));
 	}
 }
---
+++ src/main/java/com/coursejava/workshopmongodb/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.coursejava.workshopmongodb.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
