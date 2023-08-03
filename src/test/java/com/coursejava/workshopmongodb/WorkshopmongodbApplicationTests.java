c1a6dc4b84f2c97a730dc1f1df4385f41e065949
GuiWill06 <zimmerma83@gmail.com>
2023-08-03 16:43:11 -0400

    first commit

---
+++ src/test/java/com/coursejava/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursejava.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
